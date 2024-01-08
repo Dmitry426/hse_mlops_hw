8980d6316454e88523317fd18c4ab5d45250832b
DK <d.kachkin@tnxholding.com>
2024-01-08 03:22:51 +0300

    lint fix

--- hse_mlops_hw/callbacks/predictors.py
+++ hse_mlops_hw/callbacks/predictors.py
@@ -13,6 +13,7 @@ from lightning.pytorch.callbacks import BasePredictionWriter
 logger = logging.getLogger(__name__)
 
 
+# pylint: disable=unused-argument
 class CSVPredictionWriter(BasePredictionWriter, CSVLogger):
     """Csv prediction writer"""
 
